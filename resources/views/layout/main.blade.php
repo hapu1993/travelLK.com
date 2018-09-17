b657f016322dd469f3a7e97bcc87ee998e13bacf
unknown <hapuarachchi.93@gmail.com>
2018-09-17 08:14:13 -0700

    travelLK.com

---
+++ resources/views/layout/main.blade.php
@@ -0,0 +1,25 @@
+<html>
+@include('head.blade.php')
+
+<body>
+<div class="navbar navbar-inverse navbar-fixed-top">
+            <div class="navbar-inner">
+                    <div class="container">
+                        <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <a class="brand" href="#">lol</a>
+                        <div class="nav-collapse collapse">
+                            <ul class="nav pull-right">
+                                <li class="active"><a href="/">Home</a></li>
+                                <li><a href="about">About</a></li>
+                            </ul>
+                        </div>
+                    </div>
+            </div>
+        </div>
+</body>
+
+</html>
---
+++ resources/views/travelLK-home.blade.php
@@ -0,0 +1,8 @@
+@include('main.blade.php')
+@section('content')
+
+<h1>This is Test Sentence</h1>
+
+
+
+@endsection
